eb4427199f03ef0fc946aacd452264b3558d2a10
Moicr96 <moicr96@gmail.com>
2023-03-28 01:36:45 -0600

    se agregan el resto de ejercicios listos

--- main.js
+++ main.js
@@ -25,3 +25,47 @@ divAlert.innerHTML+="<li>"+libros[1]+"</li>";
 divAlert.innerHTML+="<li>"+libros[2]+"</li>";
 divAlert.innerHTML+="<li>"+libros[3]+"</li>";
 divAlert.innerHTML+="<li>"+libros[4]+"</li>";
+
+
+console.log('****************Ejercicio 2****************');
+for (let x = 1; x <= 5; x++) {
+    let dibujo = "";
+    for (let y = 1; y <= x; y++) {
+        dibujo += "* ";
+    }
+    console.log(dibujo);}
+
+console.log('****************Ejercicio 3 parte 1****************');
+xValue=10;
+while (xValue>0) {
+    console.log(xValue);
+    xValue-=0.5;
+}
+
+console.log('****************Ejercicio 3 parte 2****************');
+xValue=0;
+while (xValue<=100) {
+    if (xValue%2==0) {
+    } else {
+        console.log(xValue);
+    }   
+    xValue+=1;
+}
+console.log('****************Ejercicio 3 parte 3****************');
+nValue=6
+cont=0
+while (cont<=nValue) {
+    console.log("["+cont+"]");
+    cont++
+}
+
+
+console.log('****************Ejercicio 3 parte 4****************');
+n = 3
+conta = 1
+sum = 0;
+while (conta <= n) {
+    sum += conta;
+    conta++;
+}
+console.log(sum)
